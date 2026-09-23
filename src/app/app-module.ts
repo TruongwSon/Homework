0639d7074b74f43b6959368ac80a9c1326701aa2
TruongwSon <Sonntk24406h@st.uel.edu.vn>
2026-09-23 18:47:46 +0700

    Hoan thanh bai 14 Product Catalog

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { ServiceProductImageEvent } from './service-product-image-event/service-product-image-event';
 import { ServiceProductImageEventDetail } from './service-product-image-event-detail/service-product-image-event-detail';
+import { ProductCatalog } from './product-catalog/product-catalog';
 
 @NgModule({
-  declarations: [App, ServiceProductImageEvent, ServiceProductImageEventDetail],
+  declarations: [App, ServiceProductImageEvent, ServiceProductImageEventDetail, ProductCatalog],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
---
+++ src/app/product-catalog/product-catalog.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CatalogService } from '../services/catalog-service';
+
+@Component({
+  selector: 'app-product-catalog',
+  standalone: false,
+  styleUrl: './product-catalog.css',
+  templateUrl: './product-catalog.html',
+})
+export class ProductCatalog {
+   categories:any;
+
+  constructor(service:CatalogService){
+    this.categories = service.getCategories();
+  }
+}
